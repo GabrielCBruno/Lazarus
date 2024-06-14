0afd8a961d91bbae492cb85a250089e4c4032af4
GabrielCBruno <142371512+GabrielCBruno@users.noreply.github.com>
2024-06-14 17:25:46 -0300

    Criação ex 13

---
+++ Exercicios/Exercicio13/backup/evento.pas
@@ -0,0 +1,52 @@
+unit Evento;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TEventoF }
+
+  TEventoF = class(TForm)
+    btnCalcularTempo: TButton;
+    edtEntrada: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    lblSaida: TLabel;
+    procedure btnCalcularTempoClick(Sender: TObject);
+  private
+
+  public
+      procedure CalcularTempo ();
+  end;
+
+var
+  EventoF: TEventoF;
+
+implementation
+
+{$R *.lfm}
+
+{ TEventoF }
+
+procedure TEventoF.CalcularTempo ();
+var
+  segundos, horas, minutos : Double;
+begin
+  segundos := StrToFloat(edtEntrada.Text);
+  minutos := segundos div 60.0;
+  horas := minutos div 60.0;
+  lblSaida.Caption:= 'Horas: ' + horas.ToString + #10 + 'Minutos: ' + minutos.ToString + #10 + 'Segundos: ' + segundos.ToString;
+end;
+
+procedure TEventoF.btnCalcularTempoClick(Sender: TObject);
+begin
+   CalcularTempo();
+end;
+
+end.
+
---
+++ Exercicios/Exercicio13/prjFabrica.lpr
@@ -0,0 +1,25 @@
+program prjFabrica;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}
+  cthreads,
+  {$ENDIF}
+  {$IFDEF HASAMIGA}
+  athreads,
+  {$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms, Evento
+  { you can add units after this };
+
+{$R *.res}
+
+begin
+  RequireDerivedFormResource:=True;
+  Application.Scaled:=True;
+  Application.Initialize;
+  Application.CreateForm(TEventoF, EventoF);
+  Application.Run;
+end.
+
