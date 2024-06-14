f4148bb3059e0e4cfcb50b892569a9ab4251fb7d
GabrielCBruno <142371512+GabrielCBruno@users.noreply.github.com>
2024-06-14 16:56:11 -0300

    Finalização

--- Exercicios/Modelo/backup/modelo.pas
+++ Exercicios/Modelo/backup/modelo.pas
@@ -5,10 +5,18 @@ unit Modelo;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
 
 type
+
+  { TModeloF }
+
   TModeloF = class(TForm)
+    btnEnviar: TButton;
+    edtEntrada: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    procedure btnEnviarClick(Sender: TObject);
   private
 
   public
@@ -21,6 +29,35 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TModeloF }
+
+procedure TModeloF.btnEnviarClick(Sender: TObject);
+var
+  numero : Integer;
+  Frutas : array [0..4] of String;
+  i : Integer;
+begin
+  {Frutas[0] := 'Banana';
+  Frutas[1] := 'Mamão';
+  Frutas[2] := 'Uva';
+  Frutas[3] := 'Goiaba';
+  Frutas[4] := 'Abacate';
+
+  ShowMessage(Frutas[4]);
+  numero := 20;
+  while numero >=0 do
+  begin
+       ShowMessage('O número atual é: ' + IntToStr(numero));
+       //ShowMessage('Fruta: ' + Frutas[numero]);
+       numero:= numero - 1;
+  end;}
+  for i:=10 downto 0 do;
+  begin
+    ShowMessage('Estamos na posição: ' + IntToStr(i));
+  end;
+  ShowMessage('Acabou');
+end;
 
 end.
 
--- Exercicios/Modelo/modelo.pas
+++ Exercicios/Modelo/modelo.pas
@@ -16,6 +16,7 @@ type
     edtEntrada: TEdit;
     Label1: TLabel;
     Label2: TLabel;
+    procedure btnEnviarClick(Sender: TObject);
   private
 
   public
@@ -28,6 +29,41 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TModeloF }
+
+procedure TModeloF.btnEnviarClick(Sender: TObject);
+var
+  numero : Integer;
+  Frutas : array [0..4] of String;
+  i : Integer;
+  soma : Integer;
+begin
+  {Frutas[0] := 'Banana';
+  Frutas[1] := 'Mamão';
+  Frutas[2] := 'Uva';
+  Frutas[3] := 'Goiaba';
+  Frutas[4] := 'Abacate';
+
+  ShowMessage(Frutas[4]);
+  numero := 20;
+  while numero >=0 do
+  begin
+       ShowMessage('O número atual é: ' + IntToStr(numero));
+       //ShowMessage('Fruta: ' + Frutas[numero]);
+       numero:= numero - 1;
+  end;}
+  {for i:=10 downto 0 do
+  begin
+    ShowMessage('Estamos na posição: ' + IntToStr(i));
+  end;
+  ShowMessage('Acabou');}
+  soma := 0;
+  Repeat
+    soma := soma + 1;
+    ShowMessage(IntToStr(soma));
+  until soma = 15;
+end;
 
 end.
 
