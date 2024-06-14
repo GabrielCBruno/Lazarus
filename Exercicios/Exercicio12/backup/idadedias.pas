40a1d024271df0573d8e0a8fc05884655adf96e8
GabrielCBruno <142371512+GabrielCBruno@users.noreply.github.com>
2024-06-14 15:02:00 -0300

    Criação ex 12

---
+++ Exercicios/Exercicio12/backup/idadedias.pas
@@ -0,0 +1,62 @@
+unit IdadeDias;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TIdadeDiasF }
+
+  TIdadeDiasF = class(TForm)
+    btnConverterIdade: TButton;
+    edtIdade: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    lblSaida: TLabel;
+    procedure btnConverterIdadeClick(Sender: TObject);
+  private
+
+  public
+      function calcularIdadeDias () : Integer;
+  end;
+
+var
+  IdadeDiasF: TIdadeDiasF;
+
+implementation
+
+{$R *.lfm}
+
+{ TIdadeDiasF }
+
+function TIdadeDiasF.calcularIdadeDias () : Integer;
+var
+  idade : Integer;
+  Ano : TDateTime;
+begin
+   if edtIdade.Text = '' then
+   begin
+     idade := 0;
+     Result := idade;
+   end
+   else
+   begin
+     idade := StrToInt(edtIdade.Text);
+     Ano := Now;
+     ShowMessage(Ano);
+   end;
+end;
+
+procedure TIdadeDiasF.btnConverterIdadeClick(Sender: TObject);
+var
+  idadeDias : Integer;
+begin
+  idadeDias := calcularIdadeDias();
+end;
+
+end.
+
