681cfb9a97e7481e073fe7b65fbb737c4dc8939c
Gabriel <142371512+GabrielCBruno@users.noreply.github.com>
2024-06-16 16:10:57 -0300

    Criação ex 19

---
+++ Exercicios/Exercicio19/contagempar.pas
@@ -0,0 +1,74 @@
+unit ContagemPar;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TContagemParF }
+
+  TContagemParF = class(TForm)
+    btnGerar: TButton;
+    Label1: TLabel;
+    procedure btnGerarClick(Sender: TObject);
+  private
+
+  public
+     procedure Contar();
+  end;
+
+var
+  ContagemParF: TContagemParF;
+
+implementation
+
+{$R *.lfm}
+
+{ TContagemParF }
+
+procedure TContagemParF.Contar();
+var
+  //msg : String;
+  i : integer;
+begin
+  //msg := '';
+  for i := 0 to 10 do
+  begin
+    ShowMessage(IntToStr(i));
+    if i = 5 then
+    begin
+      break;
+    end;
+  end;
+  i := 0;
+  while i <= 10 do
+  begin
+       ShowMessage(IntToStr(i));
+       if i = 5 then
+       begin
+            break;
+       end;
+       Inc(i);
+  end;
+  i := 0;
+  repeat
+        ShowMessage(IntToStr(i));
+        if i = 5 then
+        begin
+             break;
+        end;
+        Inc(i);
+  until i > 10; //Vai ficar repetindo até que a condição seja verdadeira;
+end;
+
+procedure TContagemParF.btnGerarClick(Sender: TObject);
+begin
+   Contar();
+end;
+
+end.
+
---
+++ Exercicios/Exercicio19/prjContagemPar.lpr
@@ -0,0 +1,25 @@
+program prjContagemPar;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}
+  cthreads,
+  {$ENDIF}
+  {$IFDEF HASAMIGA}
+  athreads,
+  {$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms, ContagemPar
+  { you can add units after this };
+
+{$R *.res}
+
+begin
+  RequireDerivedFormResource:=True;
+  Application.Scaled:=True;
+  Application.Initialize;
+  Application.CreateForm(TContagemParF, ContagemParF);
+  Application.Run;
+end.
+
