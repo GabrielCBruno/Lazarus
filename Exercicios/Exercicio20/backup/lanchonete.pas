4bd93c18bb27394a7fe5c12a213cb7ccf992b047
GabrielCBruno <142371512+GabrielCBruno@users.noreply.github.com>
2024-06-17 15:52:31 -0300

    Criação ex 20

---
+++ Exercicios/Exercicio20/backup/lanchonete.pas
@@ -0,0 +1,75 @@
+unit Lanchonete;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
+  DBCtrls;
+
+type
+
+  { TLanchoneteF }
+
+  TLanchoneteF = class(TForm)
+    btnProcessarPedido: TButton;
+    CheckBoxEntrega: TCheckBox;
+    CheckGrupCardapio: TCheckGroup;
+    edtBauruOvo: TEdit;
+    edtRefrigerante: TEdit;
+    edtCheseburguer: TEdit;
+    edtHamburger: TEdit;
+    edtCachorroQuente: TEdit;
+    edtBauruSimples: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    lblSaida: TLabel;
+    procedure btnProcessarPedidoClick(Sender: TObject);
+  private
+
+  public
+      procedure processarPedido ();
+      procedure popularArray();
+  end;
+
+var
+  LanchoneteF: TLanchoneteF;
+  editsQuant : array[0..5] of Tedit;
+
+implementation
+
+{$R *.lfm}
+
+{ TLanchoneteF }
+
+procedure TLanchoneteF.popularArray();
+var
+  i : integer;
+begin
+   editsQuant[0] := edtCachorroQuente;
+   editsQuant[1] := edtBauruSimples;
+   editsQuant[2] := edtBauruOvo;
+   editsQuant[3] := edtHamburger;
+   editsQuant[4] := edtCheseburguer;
+   editsQuant[5] := edtRefrigerante;
+   for i := 0 to 5 do
+   begin
+     ShowMessage(editsQuant[i].Text);
+   end;
+end;
+
+procedure TLanchoneteF.processarPedido ();
+begin
+
+end;
+
+procedure TLanchoneteF.btnProcessarPedidoClick(Sender: TObject);
+begin
+   popularArray();
+   //processarPedido();
+end;
+
+end.
+
