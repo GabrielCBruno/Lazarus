6f9947bad63bcde71fea80e1d61d49f2ac8ac161
Gabriel <142371512+GabrielCBruno@users.noreply.github.com>
2024-06-17 20:55:09 -0300

    Finalização ex 20

--- Exercicios/Exercicio20/backup/lanchonete.pas
+++ Exercicios/Exercicio20/backup/lanchonete.pas
@@ -9,13 +9,17 @@ uses
   DBCtrls;
 
 type
+  PedidoDados = record
+         editsQuant : array[0..5] of Tedit;
+         valor : Double;
+  end;
 
   { TLanchoneteF }
 
   TLanchoneteF = class(TForm)
     btnProcessarPedido: TButton;
     CheckBoxEntrega: TCheckBox;
-    CheckGrupCardapio: TCheckGroup;
+    ckgCardapio: TCheckGroup;
     edtBauruOvo: TEdit;
     edtRefrigerante: TEdit;
     edtCheseburguer: TEdit;
@@ -27,6 +31,7 @@ type
     Label3: TLabel;
     lblSaida: TLabel;
     procedure btnProcessarPedidoClick(Sender: TObject);
+    //procedure FormCreate(Sender: TObject);
   private
 
   public
@@ -36,7 +41,7 @@ type
 
 var
   LanchoneteF: TLanchoneteF;
-  editsQuant : array[0..5] of Tedit;
+  pedido : array[0..5] of PedidoDados;
 
 implementation
 
@@ -48,18 +53,29 @@ procedure TLanchoneteF.popularArray();
 var
   i : integer;
 begin
-   editsQuant[0] := edtCachorroQuente;
-   editsQuant[1] := edtBauruSimples;
-   editsQuant[2] := edtBauruOvo;
-   editsQuant[3] := edtHamburger;
-   editsQuant[4] := edtCheseburguer;
-   editsQuant[5] := edtRefrigerante;
+   pedido[0].editsQuant[0] := edtCachorroQuente;
+   pedido[0].valor:= 12.00;
+
+   pedido[1].editsQuant[1] := edtBauruSimples;
+   pedido[1].valor:= 13.00;
+
+   pedido[2].editsQuant[2] := edtBauruOvo;
+   pedido[2].valor:= 15.00;
+
+   pedido[3].editsQuant[3] := edtHamburger;
+   pedido[3].valor := 12.00;
+
+   pedido[4].editsQuant[4] := edtCheseburguer;
+   pedido[4].valor := 13.00;
+
+   pedido[5].editsQuant[5] := edtRefrigerante;
+   pedido[5].valor := 5.00;
    //Validação dos campos
    for i := 0 to 5 do
    begin
-     if editsQuant[i].Text = '' then
+     if pedido[i].editsQuant[i].Text = '' then
      begin
-       editsQuant[i].Text:= IntToStr(0);
+       pedido[i].editsQuant[i].Text:= IntToStr(0);
      end;
    end;
 end;
@@ -67,14 +83,31 @@ end;
 procedure TLanchoneteF.processarPedido ();
 var
   Total : Double;
+  i : Integer;
+  msg : String;
 begin
      Total := 0;
+     for i := 0 to 5 do
+     begin
+       if ckgCardapio.Checked[i] = true then
+       begin
+            Total := Total + (pedido[i].valor * StrToInt(pedido[i].editsQuant[i].Text));
+       end;
+     end;
+     if CheckBoxEntrega.Checked = true then
+     begin
+          //Caso o cliente opte por receber o pedido, terá que pagar R$ 4,00 pela entrega.
+          if Total <> 0 then
+             Total := Total + 4.0;
+     end;
+     msg := 'R$ ' + FloatToStr(Total) + ',00.';
+     lblSaida.Caption:= msg;
 end;
 
 procedure TLanchoneteF.btnProcessarPedidoClick(Sender: TObject);
 begin
    popularArray();
-   //processarPedido();
+   processarPedido();
 end;
 
 end.
