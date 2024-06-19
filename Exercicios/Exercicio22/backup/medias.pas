3e48e3d2e16f1cc0b0b48c934395e49e84ca3bf7
Gabriel <142371512+GabrielCBruno@users.noreply.github.com>
2024-06-19 20:40:43 -0300

    Correção
    Realizar teste de mesa

--- Exercicios/Exercicio22/backup/medias.pas
+++ Exercicios/Exercicio22/backup/medias.pas
@@ -61,24 +61,51 @@ function TmediasF.calcularMedia () : Double;
 var
   Total : Double;
   i : Integer;
+  peso : array [0..2] of Integer;
 begin
      Total := 0;
      if cmbMedia.Text = 'Aritmética' then
      begin
+          //Média Aritmética
           for i := 0 to 2 do
           begin
                Total := Total + StrToFloat(edits[i].Text);
           end;
           Total := Total/3;
-          Reult := Total;
+          Result := Total;
+     end;
+     if cmbMedia.Text = 'Ponderada' then
+     begin
+          //Média Ponderada
+          peso[0] := 3;
+          peso[1] := 3;
+          peso[2] := 4;
+          for i := 0 to 2 do
+          begin
+            Total := Total + (StrToFloat(edits[i].Text) * peso[i]);
+          end;
+          Total := Total/(peso[0] + peso[1] + peso[2]);
+          Result := Total;
+     end;
+     if cmbMedia.Text = 'Harmônica' then
+     begin
+          //Media Harmônica
+          for i := 0 to 2 do
+          begin
+            Total := Total + (1/StrToFloat(edits[i].Text));
+          end;
+          Total := 3/Total;
+          Result := Total;
      end;
 end;
 
 procedure TMediasF.btnCalcularMediaClick(Sender: TObject);
+var
+  res : Double;
 begin
      validarEdits();
-     calcularMedia();
-
+     res := calcularMedia();
+     lblSaida.Caption := 'Resultado: ' + FloatToStr(res);
 end;
 
 end.
