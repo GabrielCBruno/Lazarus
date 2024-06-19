cfa7af678d4f357dddf05d940117b8b4cddaa006
GabrielCBruno <142371512+GabrielCBruno@users.noreply.github.com>
2024-06-19 14:48:46 -0300

    Criação ex 22

---
+++ Exercicios/Exercicio22/backup/medias.pas
@@ -0,0 +1,26 @@
+unit Medias;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TMediasF = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  MediasF: TMediasF;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
---
+++ Exercicios/Exercicio22/medias.pas
@@ -0,0 +1,30 @@
+unit Medias;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TMediasF }
+
+  TMediasF = class(TForm)
+    Label1: TLabel;
+  private
+
+  public
+
+  end;
+
+var
+  MediasF: TMediasF;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
