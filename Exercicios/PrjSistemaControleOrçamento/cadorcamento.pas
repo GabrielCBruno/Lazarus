f57fba120cc5f31cbf8ae64b431b29a383b08761
Gabriel <142371512+GabrielCBruno@users.noreply.github.com>
2024-07-03 23:33:32 -0300

    add

--- Exercicios/PrjSistemaControleOrçamento/cadorcamento.pas
+++ Exercicios/PrjSistemaControleOrçamento/cadorcamento.pas
@@ -42,21 +42,84 @@ type
     procedure DBGridPrincipalDblClick(Sender: TObject);
     procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
     procedure FormShow(Sender: TObject);
+    procedure pagCadastroShow(Sender: TObject);
+    procedure pagPesquisaShow(Sender: TObject);
     procedure SpeedButton1Click(Sender: TObject);
   private
 
   public
-
+      procedure AbreOrcItens(orcamentoid : Integer);
+      procedure povoarArray ();
+      function validarCampos () : Boolean;
   end;
 
 var
   CadOrcamentoF: TCadOrcamentoF;
+  vetor : array of TDBEdit;
 
 implementation
 
 {$R *.lfm}
 
 { TCadOrcamentoF }
+
+procedure TCadOrcamentoF.AbreOrcItens(orcamentoid : Integer);
+begin
+  if orcamentoid <> 0 then
+  begin
+      DataModule1.qryOrcamentoItens.Close;
+      DataModule1.qryOrcamentoItens.SQL.Clear;
+      DataModule1.qryOrcamentoItens.SQL.Add(
+                      'SELECT '+
+                      'ORCAMENTOITEMID, '+
+                      'ORCAMENTOID, '+
+                      'PRODUTOID, '+
+                      'produtodesc, '+
+                      'QT_PRODUTO, '+
+                      'VL_UNITARIO, '+
+                      'VL_TOTAL '+
+                      'FROM ORCAMENTO_ITEM ' +
+                      'WHERE ORCAMENTOID = '+ inttostr(orcamentoid) + ' ' +
+                      'ORDER BY ORCAMENTOID');
+       DataModule1.qryOrcamentoItens.Open;
+  end;
+end;
+
+procedure TCadOrcamentoF.povoarArray ();
+var
+  i, Count: Integer;
+begin
+  Count := 0;
+  for i := 0 to ComponentCount - 1 do
+  begin
+    if Components[i] is TDBEdit then
+    begin
+      Inc(Count);
+      SetLength(vetor, Count);
+      vetor[Count - 1] := TDBEdit(Components[i]);
+    end;
+  end;
+end;
+
+function TCadOrcamentoF.validarCampos () : Boolean;
+var
+  i : Integer;
+  val : Boolean;
+begin
+  val := true;
+  for i := 0 to High(vetor) do
+  begin
+    if vetor[i].Text = '' then
+    begin
+      val := false;
+    end;
+  end;
+  if val = false then
+  begin
+       ShowMessage('Por favor, prencha todos os campos!');
+  end;
+  Result := val;
+end;
 
 procedure TCadOrcamentoF.bitbtnCancelarClick(Sender: TObject);
 begin
@@ -72,8 +135,14 @@ end;
 
 procedure TCadOrcamentoF.bitbtnExcluirClick(Sender: TObject);
 begin
-  DataModule1.qryOrcamento.Close;
-  pagPrincipal.ActivePage := pagPesquisa;
+  if (validarCampos() = true) then
+  begin
+      If  MessageDlg('Você tem certeza que deseja excluir o registro?',mtConfirmation,[mbyes,mbno],0)=mryes then
+      begin
+           DataModule1.qryOrcamento.Delete;
+           pagPrincipal.ActivePage := pagPesquisa;
+      end;
+  end;
 end;
 
 procedure TCadOrcamentoF.bitbtnFecharClick(Sender: TObject);
@@ -83,8 +152,11 @@ end;
 
 procedure TCadOrcamentoF.bitbtnGravarClick(Sender: TObject);
 begin
-  DataModule1.qryOrcamento.Post;
-  pagPrincipal.ActivePage := pagPesquisa;
+  if (validarCampos() = true) then
+  begin
+       DataModule1.qryOrcamento.Post;
+       pagPrincipal.ActivePage := pagPesquisa;
+  end;
 end;
 
 procedure TCadOrcamentoF.bitbtnNovoClick(Sender: TObject);
@@ -127,6 +199,7 @@ procedure TCadOrcamentoF.FormClose(Sender: TObject;
 begin
      DataModule1.qryOrcamento.Close;
      DataModule1.qryOrcamentoItens.Close;
+     SetLength(vetor, 0);
      CloseAction:= caFree;
 end;
 
@@ -135,6 +208,17 @@ begin
   DataModule1.qryOrcamento.Open;
   dmPrincipal.DataModule1.qryOrcamentoItens.Open;
   pagPrincipal.ActivePage := pagPesquisa;
+end;
+
+procedure TCadOrcamentoF.pagCadastroShow(Sender: TObject);
+begin
+  povoarArray();
+  //AbreOrcItens(StrToInt(DBeditID.Text));
+end;
+
+procedure TCadOrcamentoF.pagPesquisaShow(Sender: TObject);
+begin
+
 end;
 
 procedure TCadOrcamentoF.SpeedButton1Click(Sender: TObject);
